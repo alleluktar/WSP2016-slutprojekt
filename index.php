2f9be91c920876f6c804077ec861b6b2fa1f922c
alleluktar <simon.petar@hotmail.com>
2016-04-19 12:38:43 +0200

    Read Me Updated

--- index.php
+++ index.php
@@ -6,4 +6,20 @@
  * Time: 12:55
  */
 
-/* hej */
+<?php
+session_start();
+require('header.php');
+//Code to decide files to include from get variable.
+
+if(isset($_GET['page'])&&($_GET['page']!="")) {
+    $page=$_GET['page'];
+
+    if (file_exists("views/{$page}.php"))  {
+        require("views/{$page}.php");}
+    else require('views/main.php');
+
+}
+
+else require('views/main.php');
+
+require('footer.php');
